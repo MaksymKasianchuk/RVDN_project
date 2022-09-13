c4f4d3a603c6934ddd0df415a501698a4df55275
Maksym <maxthebest1906@gmail.com>
2022-09-13 17:52:26 +0300

    index page, regional sites page, header, footer ready

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,40 @@
-// // Import vendor jQuery plugin example
-// import '~/app/libs/mmenu/dist/mmenu.js'
+import $ from 'jquery'
+window.jQuery = $
+window.$ = $
 
 document.addEventListener('DOMContentLoaded', () => {
+	const settingsSelectors= {
+		btn:'.settings-btn', 
+		menu: '.settings-menu', 
+		open: 'show'
+	}
+	const mobileSelectors= {
+		btn:'.burger-btn', 
+		menu: '.mobile-menu', 
+		open: 'show'
+	}
 
-	// Custom JS
+	$('.settings-btn').on('click', function(){
+		$(this).siblings('.settings-menu').toggleClass('show');
+	});
 
-})
+	$('.burger-btn').on('click', function(){
+		$('.mobile-menu').toggleClass('show');
+	});
+
+	$(document).mouseup(function (e){
+        missClickHandler(settingsSelectors, e);
+		missClickHandler(mobileSelectors, e);
+    });
+
+});
+
+function missClickHandler(refs, event){
+    const { btn, menu, open} =  refs;
+    const div = $(btn); 
+    if (!div.is(event.target)
+    && $(menu).has(event.target).length === 0) {
+        $(menu).removeClass(open);
+        // div.removeClass(open);
+    }
+}
