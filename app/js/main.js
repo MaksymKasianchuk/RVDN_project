ddcda7b261f665adbd58f033a9e802752227a1cd
Maksym <maxthebest1906@gmail.com>
2022-09-15 17:35:08 +0300

    add-record, add-person pages ready, add-risk-section ready

--- app/js/main.js
+++ app/js/main.js
@@ -52,6 +52,58 @@ document.addEventListener('DOMContentLoaded', () => {
 	$('.record-info-btn').on('click', function(e){
 		$('.record-card-modal').addClass('show-modal');
 	});
+
+
+	//---------ADD-NEW-PERSON---------------
+	//-----------add-relative-btn-in-fom
+	$('.add-relative-btn').on('click', function(e){
+		const strTpl = `
+			<div class="col-12 col-md-6 col-lg-4">
+				<div class="form-group">
+					<label class="form-label">ПІБ</label>
+					<input type="text" name="newperson-relationship" id="" value="">
+				</div>
+				<div class="form-group newperson-relationship-level">
+					<label class="form-label">Рівень зв'язку</label>
+					<select name="newperson-relationship-level" id="">
+						<option value="Чоловік">Чоловік</option>
+						<option value="Дружина">Дружина</option>
+						<option value="Брат">Брат</option>
+						<option value="Сестра">Сестра</option>
+						<option value="Інше..." selected>Інше...</option>
+					</select>
+				</div>
+			</div>
+		`;
+		$(this).parents('.add-relateve-col').before(strTpl);
+	});
+
+	//---------ADD-NEW-RECORD---------------
+	//add one more victim to form
+	$('.add-victim-btn').on('click', function(e){
+		const strTpl = `
+		<div class="form-group top-line-form-group">
+			<label class="form-label">Постраждала особа</label>
+			<input type="text" name="newrecord-victim" id="" value="">
+		</div>
+		`;
+		$(this).before(strTpl);
+	});
+	//add one more agressor to form
+	$('.add-agressor-btn').on('click', function(e){
+		const strTpl = `
+		<div class="form-group">
+			<label class="form-label">Особа кривдник</label>
+			<input type="text" name="newrecord-agressor" id="" value="">
+		</div>
+		`;
+		$(this).before(strTpl);
+	});
+
+	//---------------------RISK-SECTION--------------------
+	$('.open-risk-section-btn').on('click', function(e){
+		$('.add-risk-section').toggleClass('show-section');
+	});
 });
 
 function missClickHandler(refs, event){
