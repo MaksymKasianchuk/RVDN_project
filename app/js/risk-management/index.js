0e7e223881351b09f2716be65862b514ddcc0579
Maksym <maxthebest1906@gmail.com>
2022-12-13 14:37:24 +0200

    risk-management ready

--- app/js/risk-management/index.js
+++ app/js/risk-management/index.js
@@ -1,3 +1,5 @@
+import notifications from '../notifications';
+
 function riskMan() {
     // let factorsArr = [];
     // $('#risk-factor').each(function (){
@@ -14,11 +16,6 @@ function riskMan() {
     //     if(factorsArr[i] < MFactorArr[1]) {lambdaFactVal.push(1);} else lambdaFactVal.push(0);
     // }
 
-    // let bFactVal = [];
-    // for(num in numberOfValues){
-    //     let curBFactVal = 0.9 - 0.2 * (num - 1);
-    //     bFactVal.push(curBFactVal);
-    // }
 
     // let sum1;
     // for(let i = 0; i < numberOfValues; i++){
@@ -31,16 +28,18 @@ function riskMan() {
     $('.add-risk-management-btn').on('click', function(e){
         e.preventDefault();
         const strTpl = `
-        <div class="col-12 col-md-6">
-            <div class="form-group">
-                <label class="form-label">Назва фактору Fi</label>
-                <input type="text" name="" id="" value="">
+        <div class="row factor">
+            <div class="col-12 col-md-6">
+                <div class="form-group">
+                    <label class="form-label">Назва фактору Fi</label>
+                    <input type="text" name="" class="fac-name" value="">
+                </div>
             </div>
-        </div>
-        <div class="col-12 col-md-6">
-            <div class="form-group">
-                <label class="form-label">Значення фактору Fi</label>
-                <input type="text" name="" id="" value="">
+            <div class="col-12 col-md-6">
+                <div class="form-group">
+                    <label class="form-label">Значення фактору Fi</label>
+                    <input type="text" name="" class="fac-val" value="">
+                </div>
             </div>
         </div>
         `;
@@ -49,10 +48,78 @@ function riskMan() {
 
     $('.risk-management-btn').on('click', function(e){
         e.preventDefault();
+        $('.fac-name').each(function(){
+            let val = $(this).val();
+            if(!val) {
+                notifications.emptyNotif("Не заповено поле назва фактору!");
+            }
+        });
+
+        let facValues = [];
+        $('.fac-val').each(function(){
+            let val = Number($(this).val());
+            if(!val) {
+                notifications.emptyNotif("Не заповено поле значення фактору!");
+            }
+            facValues.push(val);
+        })
+
+        let numberOfValues = facValues.length;
+
+        let lastFactor = facValues[facValues.length-1];
+
+        let bFactVal = [];
+        for(let i = 1; i<= numberOfValues; i++){
+            let curBFactVal = 0.8 - 0.2 * (i - 1);
+            bFactVal.push(Number(curBFactVal.toFixed(2)));
+        }
+
+        let pi = 1/numberOfValues;
+
+        let BRiskLevel = 0;
+        for(let i = 0; i < bFactVal.length; i++){
+            BRiskLevel += bFactVal[i] * pi;
+        }
+        
+
+        let finishBRisk = 0;
+        if(lastFactor<=0.2){
+            finishBRisk = Number((BRiskLevel - lastFactor).toFixed(2));
+        } else{
+            finishBRisk = Number((BRiskLevel + lastFactor).toFixed(2));
+        }
+
+        let finishBRiskText;
+
+        if(finishBRisk > 0 && finishBRisk <= 0.2){
+            finishBRiskText = 'незначний ризик загрози';
+        }
+        if(finishBRisk > 0.2 && finishBRisk <= 0.4){
+            finishBRiskText = 'низький ризик загрози';
+        }
+        if(finishBRisk > 0.4 && finishBRisk <= 0.6){
+            finishBRiskText = 'середній ризик загрози';
+        }
+        if(finishBRisk > 0.6 && finishBRisk <= 0.8){
+            finishBRiskText = 'високий ризик загрози';
+        }
+        if(finishBRisk > 0.8 && finishBRisk <= 1){
+            finishBRiskText = 'граничний ризик загрози';
+        }
+        // console.log(finishBRisk);
+        // console.log(finishBRiskText);
+
+
+        $('.finish-fact-val').val(finishBRisk);
+        $('.finish-fact-text').val(finishBRiskText);
+        notifications.succsessNotif('Оцінювання успішно проведено');
+
         $('.risk-management-modal').addClass('show-modal');
     });
     $(".risk-management-modal-btn").on('click', function(e){
         e.preventDefault();
+        $('.finish-fact-val').val('');
+        $('.finish-fact-text').val('');
         $('.risk-management-modal').removeClass('show-modal');
     });
 }
